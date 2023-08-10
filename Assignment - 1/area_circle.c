62312760bd7e255f4362e1c916eaee01a995afb3
Rohit Kumar Mahaur <141898177+RKMahaur@users.noreply.github.com>
2023-08-10 18:48:17 +0530

    Add files via upload

---
+++ Assignment - 1/area_circle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float a,r;
+    printf("Enter the radius of the circle : ");
+    scanf("%f",&r);
+    a=(22*r*r)/7;
+    printf("Area of Circle is %.2f",a);
+    return 0;
+}
---
+++ Assignment - 1/square_of_number.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the number : ");
+    scanf("%d",&a);
+    printf("Square of %d is %d",a,a*a);
+    return 0;
+}
