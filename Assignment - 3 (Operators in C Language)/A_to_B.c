d20f7784ea5a6ebc3f9cc785fb7738fae63fab3a
Rohit Kumar Mahaur <141898177+RKMahaur@users.noreply.github.com>
2023-08-13 16:38:58 +0530

    Delete Assignment - 3 (Operators in C Language) directory

--- Assignment - 3 (Operators in C Language)/A_to_B.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    char ch='A';
-    ch++;
-    printf("New value of ch is %c",ch);
-    return 0;
-}
--- Assignment - 3 (Operators in C Language)/Size_real_constant.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int x;
-    x=sizeof(8.7);
-    printf("Size of constant is %d",x);
-    return 0;
-}
